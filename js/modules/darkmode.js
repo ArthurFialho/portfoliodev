f5c9592caa5e640bf6750f229eafd9371c0c955d
Arthur Fialho <152504065+ArthurFialho@users.noreply.github.com>
2024-10-10 20:39:39 -0300

    Delete js directory

--- js/modules/darkmode.js
+++
@@ -1,9 +0,0 @@
-function initDarkMode() {
-    let checkbox = document.querySelector("#darkmode-id")
-    if(checkbox.checked === true) {
-        document.getElementById("label").style.backgroundColor = "#000"
-    }
-    else{
-        document.getElementById("label").style.backgroundColor = "#fff"
-    }
-}
