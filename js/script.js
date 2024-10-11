1e0f965403b9d62f7ca97889767633d3635359c5
Arthur Fialho <152504065+ArthurFialho@users.noreply.github.com>
2024-10-11 14:36:35 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+import initScrollSuave from "./modules/scroll-suave.js";
+import initAnimacaoScroll from "./modules/animacao-scroll.js";
+import initAccordion from "./modules/accordion.js";
+import initTabNav from "./modules/tabnav.js";
+import initDropDownMenu from "./modules/dropdown-menu.js";
+import initMenuMobile from "./modules/menu-mobile.js";
+import initFuncionamento from "./modules/funcionamento.js";
+
+initScrollSuave();
+initAnimacaoScroll();
+initAccordion();
+initTabNav();
+initDropDownMenu();
+initMenuMobile();
+initFuncionamento();
